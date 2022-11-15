888e3aeac410018659f84bb47d3044bcaf71e585
myp <myp880@gmail.com>
2022-11-15 11:17:31 +0900

    common style update

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+  return (
+    <div className="container product-detail">
+      <section className="section section-detail">
+        <div class="product-img-area">1</div>
+        <div class="product-data-area">2</div>
+      </section>
+      <section className="section section-banner">
+        <ul className="banner-swiper">
+          <li className="swiper-item">
+            <Link className="" to="/productdetail">
+              <em>WFC PLUS</em>
+              <b>럭셔리 첫 구매시</b>
+              <p>최대 10만원 할인 혜택</p>
+            </Link>
+          </li>
+        </ul>
+      </section>
+      <section class="section section-products-data">
+        <ul class="tabs-list">
+          <li>
+            <button class="btn">상품정보</button>
+          </li>
+          <li>
+            <button class="btn">리뷰</button>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 }
