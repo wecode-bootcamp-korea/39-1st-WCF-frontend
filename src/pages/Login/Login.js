4cce1eff785f5f6d7402950bfae9a57123b2d125
OHJUHYUNG <juuh.yung5@gmail.com>
2022-11-16 20:14:05 +0900

    ADD: 로그인 페이지 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,42 +1,30 @@
 import React from 'react';
-
+import './Login.scss';
 export default function Login() {
   return (
     <div className="container login">
-      <section className="sign-in">
-        <h1 className="login-text">로그인</h1>
-        <form className="login-form">
-          <div className="input-box">
-            <input type="text" className="user-id" placeholder="아이디" />
-          </div>
-          <div className="input-box">
-            <input
-              type="password"
-              className="password"
-              placeholder="비밀번호"
-              maxLength="20"
-            />
-          </div>
-          <button type="button">로그인</button>
-          <div className="save-id">
-            <span className="checkbox">
-              <input type="checkbox" />
-              <label>아이디 저장</label>
-            </span>
-          </div>
-          <div class="link">
-            <a href="https://www.ssfshop.com/public/member/searchMbrId">
-              아이디 찾기
-            </a>
-            <a href="https://www.ssfshop.com/public/member/searchMbrId#">
-              비밀번호 찾기
-            </a>
-            <a href="https://www.ssfshop.com/public/member/addMemberStep1">
-              회원가입
-            </a>
-          </div>
-        </form>
-      </section>
+      <h1 className="login-title">로그인</h1>
+
+      <form className="login-form">
+        <div className="login-input-box">
+          <input className="login-input" placeholder="아이디" />
+          <input className="login-input" placeholder="비밀번호" />
+        </div>
+        <button className="login-btn">로그인</button>
+      </form>
+
+      <div className="login-save-id-box">
+        <input id="login-check" type="checkbox" />
+        <label for="login-check" className="login-id-check">
+          <div className="login-save-id-text">아이디 저장</div>
+        </label>
+      </div>
+
+      <div className="login-bottom">
+        <button className="find-id">아이디 찾기</button>
+        <button className="find-pw">비밀번호 찾기</button>
+        <button className="join">회원가입</button>
+      </div>
     </div>
   );
 }
