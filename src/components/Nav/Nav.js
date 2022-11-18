166e02c4667c3c20b13291b3df73daf9509022da
Hyommm <109214539+Hyommm@users.noreply.github.com>
2022-11-18 12:00:45 +0900

    modify

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Nav() {
   return (
-    <header className='header'>
-      <div className='user-login-data'>
-        <div className='inner'></div>
+    <header className="header">
+      <div className="user-login-data">
+        <div className="inner"></div>
       </div>
-      <div className=''></div>
+      <div className=""></div>
     </header>
-  )
+  );
 }
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="slider-area">
+      <div className="slider">
+        <div className="prev-btn"></div>
+        <div className="next-btn"></div>
+        <div className="slide-item"></div>
+        <div className="slide-item"></div>
+        <div className="slide-item"></div>
+      </div>
+    </div>
+  );
 }
