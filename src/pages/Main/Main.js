caa41f021b049dbaaacbbeea9253a56436dcd771
Hyommm <109214539+Hyommm@users.noreply.github.com>
2022-11-18 13:08:18 +0900

    Modify

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,10 +5,17 @@ export default function Main() {
     <div className="slider-area">
       <div className="slider">
         <div className="prev-btn"></div>
-        <div className="next-btn"></div>
-        <div className="slide-item"></div>
-        <div className="slide-item"></div>
-        <div className="slide-item"></div>
+        <div className="next-btn-play">
+          <div className="next-btn"></div>
+          <div className="play-btn"></div>
+        </div>
+        <ul className="slide-item">
+          <li>
+            <div className="slide-item-image">d</div>
+            <div className="slide-item-image">data</div>
+            <div className="slide-item-image">data</div>
+          </li>
+        </ul>
       </div>
     </div>
   );
